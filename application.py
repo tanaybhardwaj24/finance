1b5d068585140627ed1c21dac0ef5f15f5493a9a
tanaybhardwaj24 <70639071+tanaybhardwaj24@users.noreply.github.com>
2021-01-29 12:49:59 +0530

    Update and rename app.py to application.py

--- application.py
+++ application.py
@@ -31,7 +31,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
 db = SQL(os.getenv("postgres://yimyundjhmmjvq:f28602f351dacb39632021cdbb89427650245c4ce175bea158bf14831eecec51@ec2-3-220-193-133.compute-1.amazonaws.com:5432/d7jfd61o6vfih6"))
 
 # Make sure API key is set
